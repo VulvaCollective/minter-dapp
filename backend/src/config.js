f988431d0e9b12d0c6fcd2cfbfdd33ff45e6427c
TVC <103989235+VulvaCollective@users.noreply.github.com>
2022-05-03 13:52:33 +1000

    opensea test

--- backend/src/config.js
+++ backend/src/config.js
@@ -67,7 +67,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0x1Bdf290D769445ed6F31e398271D2aB5c5CAF9c0"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0x1Bdf290D769445ed6F31e398271D2aB5c5CAF9c0"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "0x6d71d30346CdEBb8a0e7ACD1E675495F8786D522"; // If you want to manually include it
